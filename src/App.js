6bba56c5f948ee29e1d5a494ee2bdb3e46a78a7e
Dylan K <drkunkel7@gmail.com>
2021-10-05 12:33:55 -0500

    Gallery/Nav component work

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from 'react'; // Module said this should be in this file, but I added it bc it wasnt here
+import React, { useState } from 'react'; // Module said this should be in this file, but I added it bc it wasnt here
 import './App.css';
 import About from './components/About';
 import Nav from "./components/Nav";
+import Gallery from './components/Gallery';
 
 function App() {
+  const [categories] = useState([
+    {
+        name: 'commercial',
+        description: "Photos of grocery stores, food trucks, and other commercial projects"
+    },
+    { name: `portraits`, description: `Portraits of people in my life` },
+    { name: `food`, description: `Delicious delicacies` },
+    {
+        name: `landscape`,
+        description: `Fields, farmhouses, waterfalls, and the beauty of nature`
+    },
+]);
+const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
     <div> 
-    <Nav></Nav>
+    <Nav categories = {categories} setCurrentCategory = {setCurrentCategory} currentCategory = {currentCategory}></Nav>
       <main>
+        <Gallery></Gallery>
         <About></About>
       </main>
     </div>
